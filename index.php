ae1497f754bc7bead6e09cd46fbed516abb16d6d
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-27 17:04:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,31 +10,22 @@ $secretKey = 'zdraste123romanlazko';
 $data = json_decode(file_get_contents('php://input'),true);
 $type = $data['type'];
 $userId = $data['object']['user_id'];
+$text = $data['object']['text'];
 $userInfo = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids=".$userId."&access_token=".$token."&v=5.8"),true);
 $user_name = $userInfo['response'][0]['first_name'];
 
-// проверяем secretKey
-//if (strcmp($data->secret, $secretKey) !== 0 && strcmp($data->type, 'confirmation') !== 0) {return;}
-//$type = $data['type'];
-// Проверяем, что находится в поле "type"
 switch ($type) {
 
     case 'message_new':
-        // получаем id автора сообщения
         
-        //$userId = $data->object->user_id;
-        // через users.get получаем данные об авторе
-        //$userInfo = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids=113601869&access_token=0d4e9c0bba882457716f8a05be540a13a19a3741f95a8684b022dcb7d1106a13b290329d1623a9f3aaa2d&v=5.8"));
-        // Вытаскиваем имя отправителя
         
         $request_params = array(
-            'message' => 'привет,'.$user_name,
+            'message' => 'привет,'.$user_name."Твое сообщение: ".$text,
             'user_id' => $userId,
             'access_token' => $token,
             'v' => '5.8'
         );
-        $get_params = http_build_query($request_params);
-        file_get_contents('https://api.vk.com/method/messages.send?'. $get_params);
+        file_get_contents('https://api.vk.com/method/messages.send?'. http_build_query($request_params));
         echo('ok'); // Возвращаем "ok" серверу Callback API
         break;
 }
