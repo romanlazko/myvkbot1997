947ce30f01b66572b75172c99a1fc2d5e94f5feb
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-27 16:39:49 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -6,6 +6,6 @@ $request_params = array(
 'access_token' => '0d4e9c0bba882457716f8a05be540a13a19a3741f95a8684b022dcb7d1106a13b290329d1623a9f3aaa2d'
 );
 $get_params = http_build_query($request_params);
-$result = json_decode(file_get_contents('https://api.vk.com/method/users.get?'. $get_params));
+$result = json_decode(file_get_contents('https://api.vk.com/method/users.get?'. $get_params),true);
 echo($result['response']['first_name']);
 ?>
