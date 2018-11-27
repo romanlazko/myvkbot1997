064a853465f1ad89558a2940b12b916829cfa720
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-27 16:35:12 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -3,7 +3,7 @@ $user_id = 113601869;
 $request_params = array(
 'user_id' => $user_id,
 'fields' => 'bdate',
-'v' => '5.8'
+'v' => '5.8',
 'access_token' => '0d4e9c0bba882457716f8a05be540a13a19a3741f95a8684b022dcb7d1106a13b290329d1623a9f3aaa2d'
 );
 $get_params = http_build_query($request_params);
