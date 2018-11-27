18b89f62004ff24def1aac5215ccf718f5b7219f
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-27 15:21:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ switch ($data->type) {
         // Вытаскиваем имя отправителя
         $user_name = $userInfo->response[0]->first_name;
         $request_params = array(
-            'message' => 'привет,'$user_name,
+            'message' => 'привет',
             'user_id' => $userId,
             'access_token' => $token,
             'v' => '5.8'
