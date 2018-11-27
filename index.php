a24d61a5706089efdefdff2d642bf218ffc8e6c3
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-27 15:35:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ switch ($data->type) {
         // получаем id автора сообщения
         $userId = $data->object->user_id;
         // через users.get получаем данные об авторе
-        $userInfo = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$userId}&v=5.8"));
+        $userInfo = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$userId}&access_token={$token}&v=5.8"));
         // Вытаскиваем имя отправителя
         $user_name = $userInfo->response->items->first_name;
         $request_params = array(
