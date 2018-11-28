5a706507269bdee25168678d391fdbb6bc03635e
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-28 12:59:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,43 +9,44 @@ $secretKey = 'zdraste123romanlazko';
 
 $data = json_decode(file_get_contents('php://input'),true);
 $type = $data['type'];
-$userId = $data['object']['user_id'];
+$user_id = $data['object']['user_id'];
 $text = $data['object']['body'];
 $userInfo = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids=".$userId."&access_token=".$token."&v=5.8"),true);
 $user_name = $userInfo['response'][0]['first_name'];
+$keyboard = [ 
+    'one_time' => false, 
+    'buttons' => [[
+        ['action' =>['type' => 'text', 
+                     'payload' => '{"button": "1"}',
+                     'label' => 'blue', 
+                    ],
+        'color' => 'negative',],
 
-switch ($type) {
+        ['action' =>['type' => 'text',
+                     'payload' => '{"button": "2"}',
+                     'label' => 'yellow',
+                    ], 
+        'color' => 'positive',],
+    ],],
+]; 
 
-    case 'message_new':
-        $keyboard = [ 
-            'one_time' => false, 
-            'buttons' => [[
-                ['action' =>['type' => 'text', 
-                             'payload' => '{"button": "1"}',
-                             'label' => 'blue', 
-                            ],
-                'color' => 'negative',],
-
-                ['action' =>['type' => 'text',
-                             'payload' => '{"button": "2"}',
-                             'label' => 'yellow',
-                            ], 
-                'color' => 'positive',],
-            ],],
-        ];  
- 
-            
+if ($type == 'message_new'){
+    if($text=='Начать') {
+        $reply = 'Привет, '.$user_name;
+        sendMessage($token,$user_name,$user_id,$reply,$keyboard);
+    }
         
-        $request_params = array(
-            'message' => 'привет,'.$user_name."Твое сообщение: ".$text,
-            'user_id' => $userId,
-            'access_token' => $token,
-            'keyboard'=>json_encode($keyboard),
-            'v' => '5.8'
-        );
-        file_get_contents('https://api.vk.com/method/messages.send?'. http_build_query($request_params));
-        echo('ok'); // Возвращаем "ok" серверу Callback API
-        break;
 }
-
+function sendMessage($token,$user_name,$user_id,$reply,$keyboard){
+    $request_params = array(
+        'message' => $reply,
+        'user_id' => $user_id,
+        'access_token' => $token,
+        'keyboard'=>json_encode($keyboard),
+        'v' => '5.8'
+    );
+    file_get_contents('https://api.vk.com/method/messages.send?'. http_build_query($request_params));
+    echo('ok'); 
+    break;
+}
 ?>
