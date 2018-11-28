5671d0332791fc264530b98489d62e35e540834b
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-28 12:37:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,11 +19,49 @@ switch ($type) {
     case 'message_new':
         $keyboard = [ 
         'one_time' => false, 
-        'buttons' => [
-            [
-              ['action' => ['type' => 'text', 'payload' => '{"button": "1"}', 'label' => 'Red',], 'color' => 'negative',],
-            ], 
-        ];  
+  'buttons' => [ 
+    [ 
+      [ 
+        'action' =>   
+        [ 
+          'type' => 'text', 
+          'payload' => '{"button": "1"}', 
+          'label' => 'Red', 
+        ], 
+        'color' => 'negative', 
+      ], 
+      [ 
+        'action' =>   
+        [ 
+          'type' => 'text', 
+          'payload' => '{"button": "2"}', 
+          'label' => 'Green', 
+        ], 
+        'color' => 'positive', 
+      ], 
+    ], 
+    [ 
+      [ 
+        'action' =>   
+        [ 
+          'type' => 'text', 
+          'payload' => '{"button": "3"}', 
+          'label' => 'White', 
+        ], 
+        'color' => 'default', 
+      ], 
+      [ 
+        'action' =>   
+        [ 
+          'type' => 'text', 
+          'payload' => '{"button": "4"}', 
+          'label' => 'Blue', 
+        ], 
+        'color' => 'primary', 
+      ], 
+    ], 
+  ], 
+];  
  
             
         
