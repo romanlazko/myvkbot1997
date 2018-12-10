c4db6a4fef6be27fe59a451e7ac04e3e57afa614
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-10 17:09:50 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -20,7 +20,7 @@ while(1){
             echo json_encode($request);
             break 2;
         }
-        if($item[0]===[]){
+        if(!isset($item[0])){
             echo 'Время ожидания истекло';
             break 2;
         }
