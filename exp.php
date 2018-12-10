87ec30cdb7b6135fe1b95ef82e67c840cacf8656
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-10 12:50:09 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -14,19 +14,16 @@ $pool = [
     "ts" => $pool_data->response->ts
 ];
 $item[0] = '0';
-$filed = null;
-while($item[0] !='4'  ){
+$filed[0] = '0';
+while($item[0] !='4'  or $filed[0] !='2'){
     $request = json_decode(file_get_contents("https://" . $pool['server'] . "?act=a_check&key=" . $pool['key'] . "&ts=" . $pool['ts'] . "&wait=25&mode=2&version=2"));
     foreach ($request->failed as $filed){ 
         if ($filed[0] == '2'){
-            echo json_encode($request);
+            $ec = 'Время ожидания истекло';
             break;
         }
     }
     foreach ($request->updates as $item) {
-        if($item[0] == '61'){
-            echo 'write';
-        }
         if ($item[0] == "4") {
             $ec = $item[5];
             break;
