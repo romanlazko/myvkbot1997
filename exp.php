7e13901c0b6694a34ddb7e56430370093a1a3e27
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-10 13:42:21 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -17,23 +17,14 @@ $item[0] = '0';
 $filed[0] = '0';
 while($item[0] !='4' ){
     $request = json_decode(file_get_contents("https://" . $pool['server'] . "?act=a_check&key=" . $pool['key'] . "&ts=" . $pool['ts'] . "&wait=25&mode=2&version=2"));
-    $filed = $request->failed;
-    if(isset($filed)) {
-        echo json_encode($request);
-        break;
+    foreach ($request->updates as $item) {
+        if ($item[0] == "4") {
+            $ec = $item[5];
+        }       
+        if(empty($item[0])){
+            $ec = 'Время истекло';
+        }
     }
-    $item = $request->updates;
-    if(isset($item)){
-        echo json_encode($request);
-        break;
-    }
-        
-//     foreach ($request->updates as $item) {
-//         if ($item[0] == "4") {
-//             $ec = $item[5];
-//         }
-        
-//     }
 //     $filed = $request->failed;
 //     if(isset($filed)){echo $request;}
 //     if ($filed[0] = "2"){
