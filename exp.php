2c8d3682ebdcda0680df1fc56bd617c8ef3c69a8
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-10 17:30:56 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -16,7 +16,7 @@ $pool = [
 while(1){
     $request = json_decode(file_get_contents("https://" . $pool['server'] . "?act=a_check&key=" . $pool['key'] . "&ts=" . $pool['ts'] . "&wait=25&mode=2&version=2"));
     foreach ($request->updates as $item) {
-        if(isset($item)){
+        if(isset($item[0])){
             echo json_encode($request);
             break 2;
         }
