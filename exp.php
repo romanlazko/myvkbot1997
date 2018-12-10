27062a8e5d64930eae330a10c3d5f7124aa25a77
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-10 12:37:07 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -14,14 +14,15 @@ $pool = [
     "ts" => $pool_data->response->ts
 ];
 $item[0] = '0';
+$filed = null;
 while($item[0] !='4'  ){
     $request = json_decode(file_get_contents("https://" . $pool['server'] . "?act=a_check&key=" . $pool['key'] . "&ts=" . $pool['ts'] . "&wait=25&mode=2&version=2"));
     $filed = $request->failed;
-    if(isset($filed)){    
+        
     if ($filed[0] = '2'){
         echo json_encode($request);
         break;
-    }}
+    }
     foreach ($request->updates as $item) {
 //     $item = $request->updates;
         if($item[0] == '61'){
