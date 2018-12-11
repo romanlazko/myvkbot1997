b646c9d109376f2b2296ae6d0018a96f4a155731
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-11 12:54:34 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -16,7 +16,8 @@ $pool = [
 while(1){
     $request = json_decode(file_get_contents("https://" . $pool['server'] . "?act=a_check&key=" . $pool['key'] . "&ts=" . $pool['ts'] . "&wait=25&mode=2&version=2"),true);
     $item = $request['updates'][0][0];
-    if($item){if($item=='4'){
+    if(isset($item)){
+        if($item=='4'){
         echo 'сообщение';
         break;
     }}
