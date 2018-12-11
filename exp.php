52fbda9bd4c1e8b034f96886839525f654831910
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-11 12:15:38 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -15,7 +15,7 @@ $pool = [
 ];
 while(1){
     $request = json_decode(file_get_contents("https://" . $pool['server'] . "?act=a_check&key=" . $pool['key'] . "&ts=" . $pool['ts'] . "&wait=25&mode=2&version=2"),true);
-    $item = $request['updates']['0'];
+    $item = $request['updates'][0];
     
     echo json_encode($item);
     break;
