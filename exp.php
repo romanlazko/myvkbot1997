7ed53c863d87e911e13663cc65450a7e3e489b8f
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-11 13:34:24 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -13,14 +13,14 @@ $pool = [
     "server" => $pool_data->response->server,
     "ts" => $pool_data->response->ts
 ];
-$bool==FALSE;
-while($bool!=TRUE){
+$bool===FALSE;
+while($bool!==TRUE){
     $request = json_decode(file_get_contents("https://" . $pool['server'] . "?act=a_check&key=" . $pool['key'] . "&ts=" . $pool['ts'] . "&wait=25&mode=2&version=2"));
     foreach ($request->updates as $item) {
     
         if ($item[0] == "4") {
             echo $item[5];
-            $bool == TRUE;
+            $bool === TRUE;
         } 
     }
 //     $item = $request['updates'][0][0];
