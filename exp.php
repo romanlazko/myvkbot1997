a970e498a3de567c775f6329041e8738baca1e16
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-11 12:00:16 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -14,10 +14,10 @@ $pool = [
     "ts" => $pool_data->response->ts
 ];
 while(1){
-    $request = json_decode(file_get_contents("https://" . $pool['server'] . "?act=a_check&key=" . $pool['key'] . "&ts=" . $pool['ts'] . "&wait=25&mode=2&version=2"));
-    $item = $request->updates;
+    $request = json_decode(file_get_contents("https://" . $pool['server'] . "?act=a_check&key=" . $pool['key'] . "&ts=" . $pool['ts'] . "&wait=25&mode=2&version=2"),true);
+    $item = $request['updates'];
     
-    echo json_encode($item[1]);
+    echo $item[1];
     break;
 //             if($item == []){
 //                 echo $item;            
