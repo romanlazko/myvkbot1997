b518c4f273123bd11c0fd91b41bf7dcf60b7192a
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-14 17:26:44 +0100

    Update bd.php

--- bd.php
+++ bd.php
@@ -1,7 +1,22 @@
 <?php
-$orderInsert = "INSERT INTO order_id(user_id,id,order_text,order_time) VALUES('$user_id','$id','$pos_id','$order_time')";            
-        if($dbconnect->query($orderInsert) === TRUE){
-            $reply_restoran = "Стол: ".$table."\nЗаказ: ".$pos_id."\nКоличество: ".$pos_name; 
-            inlineKeyboard($restoran,$chat_id,$reply_restoran,confirm($table,$pos_name,$pos_id));
+function name($user_id){ 
+    $servername="db4free.net: 3306";
+    $username="romanlazko";
+    $password="zdraste123";    
+    $dbname="promocoder1";
+    $dbconnect = new mysqli($servername, $username, $password, $dbname);
+    $result = $dbconnect->query("SELECT user_id FROM vkbot");    
+    while($row = $result->fetch_assoc()){        
+        if($row['user_id']==$user_id){
+            $new_id = false;
+            break;
         }
+    }   
+    if($new_id !== false){
+        $insertname = $dbconnect->query("INSERT INTO vkbot(user_id,disen) VALUES('$user_id','enable')");
+    }else{
+        $insertname = $dbconnect->query("UPDATE 'vkbot' SET 'disen'='enable' WHERE 'user_id'='user_id'");
+    }
+        
+}
 ?>
