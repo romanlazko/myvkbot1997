3fb1f87d6d00e4eec616ca9a34fb53170e1aca3c
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-14 18:46:03 +0100

    Update bd.php

--- bd.php
+++ bd.php
@@ -38,6 +38,7 @@ function setdisen($user_id){
     $result = $dbconnect->query("SELECT disen FROM vkbot WHERE user_id='$user_id'");    
     while($row = $result->fetch_assoc()){        
         if($row['disen']==1){
+            $updatename = $dbconnect->query("UPDATE `vkbot` SET `disen`='0' WHERE `user_id`='$user_id'");
             return true;
             break;
         }
