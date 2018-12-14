5ccad5e8e371b4e6a57b1ce316d3d01147c51006
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-14 18:54:22 +0100

    Update bd.php

--- bd.php
+++ bd.php
@@ -1,40 +1,26 @@
 <?php
-function name($token,$user_id,$reply){ 
-    $servername="db4free.net: 3306";
-    $username="romanlazko";
-    $password="zdraste123";    
-    $dbname="promocoder1";
-    $dbconnect = new mysqli($servername, $username, $password, $dbname);
+function name($token,$user_id,$reply,$dbconnect){ 
     $result = $dbconnect->query("SELECT user_id FROM vkbot");    
     while($row = $result->fetch_assoc()){        
         if($row['user_id']==$user_id){
             $new_id = false;
             break;
         }
-        
     }   
     if($new_id !== false){
         $insertname = "INSERT INTO vkbot(user_id,disen,name,surname) VALUES('$user_id','1','1','1')";
         if($dbconnect->query($insertname)===TRUE){
             sendMessage($token,$user_id,$reply);
-            $dbconnect->close();
         }
     }else{
         $updatename = "UPDATE `vkbot` SET `disen`='1' WHERE `user_id`='$user_id'";
         if($dbconnect->query($updatename)===TRUE){
             sendMessage($token,$user_id,$reply);
-            $dbconnect->close();
         }
     }
-    
-        
 }
-function setdisen($user_id){ 
-    $servername="db4free.net: 3306";
-    $username="romanlazko";
-    $password="zdraste123";    
-    $dbname="promocoder1";
-    $dbconnect = new mysqli($servername, $username, $password, $dbname);
+function setdisen($user_id,$dbconnect){ 
+    
     $result = $dbconnect->query("SELECT disen FROM vkbot WHERE user_id='$user_id'");    
     while($row = $result->fetch_assoc()){        
         if($row['disen']==1){
@@ -42,8 +28,6 @@ function setdisen($user_id){
             return true;
             break;
         }
-        
     }   
-    $dbconnect->close();
 }
 ?>
