2cfc56bf00c2800a29b19a13603887e1cdc49163
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-14 20:14:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -34,15 +34,13 @@ function setdisen($user_id){
     $dbname="promocoder1";
     $dbconnect = new mysqli($servername, $username, $password, $dbname);
     $result1 = $dbconnect->query("SELECT disen FROM vkbot WHERE user_id='$user_id'");    
-    while($row = $result1->fetch_assoc()){        
+    $row = $result1->fetch_assoc();       
         if($row['disen']=='1'){
             $updatename1 = $dbconnect->query("UPDATE `vkbot` SET `disen`='0' WHERE `user_id`='$user_id'");
             return true;
-            break;
         }
         else{
             return false;
-            break;
         }
     }   
     $dbconnect->close();
