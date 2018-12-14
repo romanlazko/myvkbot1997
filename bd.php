cc3b560134b75f7e7d269aabd73b5ea28534791e
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-14 17:51:18 +0100

    Update bd.php

--- bd.php
+++ bd.php
@@ -15,11 +15,13 @@ function name($token,$user_id,$reply){
     if($new_id !== false){
         $insertname = $dbconnect->query("INSERT INTO vkbot(user_id,disen,name,surname) VALUES('$user_id','1','1','1')");
         if($insertname){
+            $reply = ", что бы проверить почту, отправь мне свое имя и фамилию по паспорту";
             sendMessage($token,$user_id,$reply);
         }
     }else{
         $insertname = $dbconnect->query("UPDATE `vkbot` SET `disen`='1' WHERE `user_id`='user_id'");
         if($insertname){
+            $reply = ", что бы проверить почту, отправь мне свое имя и фамилию по паспорту";
             sendMessage($token,$user_id,$reply);
         }
     }
