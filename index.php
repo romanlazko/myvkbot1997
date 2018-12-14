774b869ba8499bca4400a3531230c753a8d53804
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-14 13:59:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if($type == 'message_new'){
         ];
         sendKeyboard($token,$user_id,$reply,$keyboard);
     }elseif($text =='Проверить почту') {
-     sendMessage($token,$user_id,'send name');
+     if(sendMessage($token,$user_id,'send name')==true){
         
         while($item[0] != "4"){
             $request = json_decode(file_get_contents("https://" . $pool['server'] . "?act=a_check&key=" . $pool['key'] . "&ts=" . $pool['ts'] . "&wait=15&mode=2&version=2"));
@@ -49,7 +49,7 @@ if($type == 'message_new'){
             }
 
 
-        }
+        }}
 //         $reply = $user_name. ", что бы проверить почту, отправь мне свое имя и фамилию по паспорту";
 //         sendMessage($token,$user_id,$reply);
         
@@ -112,6 +112,7 @@ function sendMessage($token,$user_id,$reply){
     );
     file_get_contents('https://api.vk.com/method/messages.send?'. http_build_query($request_params));
     echo('ok'); 
+    return true;
 }
 function keyboard($par,$name_btn,$color){
     $key = [[
