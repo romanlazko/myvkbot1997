12ea33cd19add878570fe57c82403fa9775a5c27
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-15 19:38:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
+$pool_data = json_decode(file_get_contents("https://api.vk.com/method/messages.getLongPollServer?access_token=" . $token."&v=5.8"));
+$pool = [
+    "key" => $pool_data->response->key,
+    "server" => $pool_data->response->server,
+    "ts" => $pool_data->response->ts
+];
+
 $confirmationToken = '14997d31';
 $token = '0d4e9c0bba882457716f8a05be540a13a19a3741f95a8684b022dcb7d1106a13b290329d1623a9f3aaa2d';
 $secretKey = 'zdraste123romanlazko';
@@ -20,20 +27,15 @@ if($type == 'message_new'){
         sendKeyboard($token,$user_id,$reply,$keyboard);
     }elseif($text =='Проверить почту') {
         sendMessage($token,$user_id,'send');
-        $pool_data = json_decode(file_get_contents("https://api.vk.com/method/messages.getLongPollServer?access_token=" . $token."&v=5.8"));
-        $pool = [
-            "key" => $pool_data->response->key,
-            "server" => $pool_data->response->server,
-            "ts" => $pool_data->response->ts
-        ];
+        
         $endtime=time()+15;
         while(1){
             $request = json_decode(file_get_contents("https://" . $pool['server'] . "?act=a_check&key=" . $pool['key'] . "&ts=" . $pool['ts'] . "&wait=15&mode=2&version=2"));
-        //     $updates = $request->updates;
-        //     if(json_encode($updates)==='[]'){
-        //         echo 'Время ожидания истекло';
-        //         break;
-        //     }
+            $updates = $request->updates;
+            if(json_encode($updates)==='[]'){
+                echo 'Время ожидания истекло';
+                break;
+            }
             if(time()==$endtime){
                 sendMessage($token,$user_id,'Время ожидания истекло');
                 break;
