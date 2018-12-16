26ea987417fac2b3d3a8be8f8e25f0d95b85ca45
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-16 18:49:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $user_id = $data['object']['user_id'];
 $text = $data['object']['body'];
 $userInfo = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids=".$user_id."&access_token=".$token."&v=5.8"),true);
 $user_name = $userInfo['response'][0]['first_name'];
-// sendMessage($token,$user_id,$type);
+sendMessage($token,$user_id,$type);
 if($type == 'message_reply'){
     if($text =='send') {
         $pool_data = json_decode(file_get_contents("https://api.vk.com/method/message.getLongPollServer?access_token=" . $token."&v=5.8"));
