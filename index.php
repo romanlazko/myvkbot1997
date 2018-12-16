f6fb287fbfab5e30dfb5d4a653d3ae8c364f745c
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-16 20:59:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -132,6 +132,6 @@ function keyboard($par,$name_btn,$color){
         
 //         //sendKeyboard($token,$user_id,$reply,$keyboard);
 //     }
-$dbconnect->close();
+// $dbconnect->close();
 // return false;
 ?>
