b37ade616f72aaf5f12328a821854f81ef026cda
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-24 00:54:05 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -3,7 +3,7 @@ $row = 1;
 if (($handle = fopen("Prehled_k_17-12-2018.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
-        echo "<p> $num полей в строке $row: <br /></p>\n";
+        
         $row++;
         for ($c=0; $c < $num; $c++) {
             echo $data[$c] . "<br />\n";
