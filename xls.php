1cfbc0635513ce1ddf1a0ced6e94d20c59660efa
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-24 14:10:14 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -2,7 +2,7 @@
 $row = 1;
 if (($handle = fopen("test2.csv", "r")) !== FALSE) {
     $data = fgetcsv($handle, 1000, ",");
-    while ($data !== FALSE) {
+    //while ($data !== FALSE) {
         $num = count($data);
         
         $row++;
@@ -17,7 +17,7 @@ if (($handle = fopen("test2.csv", "r")) !== FALSE) {
 //                 break 2;
 //             }
         //}
-    }
+    //}
     fclose($handle);
 }
 ?>
