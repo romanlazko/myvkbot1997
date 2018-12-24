63a5224f9e2af5f50c709553b54daee2f1ac387c
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-24 14:09:00 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -2,12 +2,12 @@
 $row = 1;
 if (($handle = fopen("test2.csv", "r")) !== FALSE) {
     $data = fgetcsv($handle, 1000, ",");
-    //while ($data !== FALSE) {
+    while ($data !== FALSE) {
         $num = count($data);
         
         $row++;
         //for ($c=0; $c < 5; ) {
-            echo $data."<br />\n";
+            echo $data[0]."<br />\n";
 //             $pos      = strripos($data[$c], 'bAM-15898/DP-2018');
 
 //             if ($pos === false) {
@@ -17,7 +17,7 @@ if (($handle = fopen("test2.csv", "r")) !== FALSE) {
 //                 break 2;
 //             }
         //}
-   // }
+    }
     fclose($handle);
 }
 ?>
