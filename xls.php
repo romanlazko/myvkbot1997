93aa6e42b7647d6b85b83d6770680afbedb947fe
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-26 19:28:07 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
+header('Content-Type: text/html; charset=windows-1251');
 $row = 1;
 if (($handle = fopen("test2.csv", "r")) !== FALSE) {
      
