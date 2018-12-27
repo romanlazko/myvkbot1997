94aac4844c749050c890f9080e3826a9284b674f
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-27 12:45:50 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -2,7 +2,7 @@
 
 header('Content-Type: text/html; charset=ISO-8859-2');
 $row = 1;
-if (($handle = fopen("https://vk.com/doc198479020_486012686?hash=f23217a8ce1249e853&dl=591a8841aeba4034e2", "r")) !== FALSE) {
+if (($handle = fopen("https:\/\/vk.com\/doc198479020_486014179?hash=2d06958b8235974c68&dl=GE4TQNBXHEYDEMA:1545911019:2d35238ca9f87c6505&api=1&no_preview=1", "r")) !== FALSE) {
      while (($data= fgetcsv($handle, 1000, ",")) !== FALSE) {
         
           $text1 = substr($data[1], 2);
