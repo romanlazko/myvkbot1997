b968e3dd6a4ec9a15bc20f6e20570d902380739c
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-27 12:03:31 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -7,11 +7,11 @@ if (($handle = fopen("Prehled_k_17-12-2018.csv", "r")) !== FALSE) {
           $text1 = substr($data[1], 2);
           $text = substr($text1, 0, -2);
           if($text==='28112-5/DP-2016'){
-               echo 'find';
+               echo 'find1';
                break;
           }
           elseif(strripos($data[1], '28112/DP-2016')){
-               echo 'find';
+               echo 'find2';
                break;
           }
           else{
