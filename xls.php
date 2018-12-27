b693cd0c847ab3c750770952423ab37e881422f1
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-27 12:20:35 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -4,31 +4,20 @@ header('Content-Type: text/html; charset=utf-8');
 $row = 1;
 if (($handle = fopen("Prehled_k_17-12-2018.xls", "r")) !== FALSE) {
      while (($data= fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $num = count($data);
-        $row++;
-        for ($c=0; $c < $num; $c++) {
-          if(strripos($data[$c], 'OAM-20007/PP-2018')){
-               echo 'find2: '.$data[$c];
+        
+          $text1 = substr($data[1], 2);
+          $text = substr($text1, 0, -2);
+          if($text==='28112-5/DP-2016'){
+               echo 'find1';
                break;
           }
-          else{     
+          elseif(strripos($data[1], '28112/DP-2016')){
+               echo 'find2: '.$data[1];
+               break;
+          }
+          else{
                continue;
           }
-            //echo $data[$c] . "<br />\n";
-        }
-//           $text1 = substr($data[1], 2);
-//           $text = substr($text1, 0, -2);
-//           if($text==='28112-5/DP-2016'){
-//                echo 'find1';
-//                break;
-//           }
-//           elseif(strripos($data[1], '28112/DP-2016')){
-//                echo 'find2: '.$data[1];
-//                break;
-//           }
-//           else{
-//                continue;
-//           }
      }
 fclose($handle);
 }
