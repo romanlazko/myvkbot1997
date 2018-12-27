561becf83d804e8150db964ae145364077902770
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-27 12:47:08 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -2,7 +2,7 @@
 
 header('Content-Type: text/html; charset=ISO-8859-2');
 $row = 1;
-if (($handle = fopen("https:\/\/vk.com\/doc198479020_486014179?hash=2d06958b8235974c68&dl=GE4TQNBXHEYDEMA:1545911019:2d35238ca9f87c6505&api=1&no_preview=1", "r")) !== FALSE) {
+if (($handle = fopen("https:\\vk.com\doc198479020_486014179?hash=2d06958b8235974c68&dl=GE4TQNBXHEYDEMA:1545911019:2d35238ca9f87c6505&api=1&no_preview=1", "r")) !== FALSE) {
      while (($data= fgetcsv($handle, 1000, ",")) !== FALSE) {
         
           $text1 = substr($data[1], 2);
