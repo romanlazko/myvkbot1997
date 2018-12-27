e1922d95cef4ad9deda800ea32b8d81ad063304c
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-27 11:30:37 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -10,7 +10,7 @@ if (($handle = fopen("Prehled_k_17-12-2018.csv", "r")) !== FALSE) {
 //           if($text='--OAM-28112/DP-2016--'){
 //                echo 'нашелся';
 //           }
-          $text = substr($data[1], 0, -2);
+          $text = substr($data[1], 2, -2);
           echo $text.".<br />\n";
      }
 fclose($handle);
