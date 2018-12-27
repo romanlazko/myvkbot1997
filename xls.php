9367c7756b02e15dfa6c30ccc62ef5cfd8877288
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-27 12:11:39 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -4,7 +4,12 @@ header('Content-Type: text/html; charset=utf-8');
 $row = 1;
 if (($handle = fopen("Prehled_k_17-12-2018.xls", "r")) !== FALSE) {
      while (($data= fgetcsv($handle, 1000, ",")) !== FALSE) {
-        echo $data[1];
+        $num = count($data);
+        echo "<p> $num fields in line $row: <br /></p>\n";
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            echo $data[$c] . "<br />\n";
+        }
 //           $text1 = substr($data[1], 2);
 //           $text = substr($text1, 0, -2);
 //           if($text==='28112-5/DP-2016'){
