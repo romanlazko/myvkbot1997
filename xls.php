6d9bd596727c44698d777740883026f07e849f08
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-27 12:34:03 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -7,11 +7,11 @@ if (($handle = fopen("https://vk.com/doc198479020_486012686?hash=f23217a8ce1249e
         
           $text1 = substr($data[1], 2);
           $text = substr($text1, 0, -2);
-          if($text==='28112-5/DP-2016'){
+          if($text==='OAM-20104/PP-2018'){
                echo 'find1';
                break;
           }
-          elseif(strripos($data[1], '28112/DP-2016')){
+          elseif(strripos($data[1], 'OAM-20104/PP-2018')){
                echo 'find2: '.$data[1];
                break;
           }
