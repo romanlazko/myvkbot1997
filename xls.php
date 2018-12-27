cb90cfcb2ce82fdb045f108d03d5ec0c60a82245
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-27 12:02:43 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -3,18 +3,20 @@ header('Content-Type: text/html; charset=ISO-8859-2');
 $row = 1;
 if (($handle = fopen("Prehled_k_17-12-2018.csv", "r")) !== FALSE) {
      while (($data= fgetcsv($handle, 1000, ",")) !== FALSE) {
-//           if($num < 2){
-//                continue;
-//           }
-          //$text = str_replace(' ','-',$data[1]);
-//           
+        
           $text1 = substr($data[1], 2);
           $text = substr($text1, 0, -2);
-          if($text==='OAM-28112/DP-2016'){
+          if($text==='28112-5/DP-2016'){
                echo 'find';
+               break;
           }
-          else{continue;}
-//           echo $text.".<br />\n";
+          elseif(strripos($data[1], '28112/DP-2016')){
+               echo 'find';
+               break;
+          }
+          else{
+               continue;
+          }
      }
 fclose($handle);
 }
