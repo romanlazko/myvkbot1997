aee8e1706a54d68ad5d8a435f5ce6dae50660345
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-28 14:43:18 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -1,5 +1,17 @@
 <?php
-
+// $cutvisa = substr($visanum[1], 2);
+//                 $cutvisa1 = substr($cutvisa, 0, -2);
+//                 if($cutvisa1==='OAM-20104/PP-2018'){
+//                      sendMessage($token,$user_id,$visanum[1]);
+//                      break;
+//                 }
+//                 elseif(strripos($visanum[1], 'OAM-20104/PP-2018')){
+//                      sendMessage($token,$user_id,$visanum[1]);
+//                      break;
+//                 }
+//                 else{
+//                      continue;
+//                 }
 header('Content-Type: text/html; charset=ISO-8859-2');
 $row = 1;
 if (($handle = fopen("https://vk.com/doc198479020_486014179?hash=2d06958b8235974c68&dl=GE4TQNBXHEYDEMA:1545911019:2d35238ca9f87c6505&api=1&no_preview=1", "r")) !== FALSE) {
