3b22f370d9aaee0ebf4112eec45a330d6add900a
romanlazko <43849612+romanlazko@users.noreply.github.com>
2019-01-02 22:45:57 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -29,7 +29,7 @@ function select_file(){
 header('Content-Type: text/html; charset=ISO-8859-2');
 $row = 1;
 
-if (($handle = fopen('https://vk.com/doc113601869_486322806?hash=2d06958b8235974c68', "r")) !== FALSE) {
+if (($handle = fopen("https://vk.com/doc113601869_486322806?hash=2d06958b8235974c68", "r")) !== FALSE) {
      while (($data= fgetcsv($handle, 1000, ",")) !== FALSE) {
         
           $text1 = substr($data[1], 2);
