00d7d3db6b37b4b863e44adf130d774331c2d152
romanlazko <43849612+romanlazko@users.noreply.github.com>
2019-01-02 22:47:57 +0100

    Update xls.php

--- xls.php
+++ xls.php
@@ -29,16 +29,16 @@ function select_file(){
 header('Content-Type: text/html; charset=ISO-8859-2');
 $row = 1;
 
-if (($handle = fopen("https://vk.com/doc113601869_486322806?hash=2d06958b8235974c68", "r")) !== FALSE) {
+if (($handle = fopen("Prehled_k_17-12-2018.csv", "r")) !== FALSE) {
      while (($data= fgetcsv($handle, 1000, ",")) !== FALSE) {
         
           $text1 = substr($data[1], 2);
           $text = substr($text1, 0, -2);
-          if($text==='OAM-20104/PP-201'){
+          if($text==='OAM-20104/PP-2018'){
                echo 'find1';
                break;
           }
-          elseif(strripos($data[1], 'OAM-20104/PP-2018')){
+          elseif(strripos($data[1], 'OAM-20104/PP-201')){
                echo 'find2: '.$data[1];
                break;
           }
