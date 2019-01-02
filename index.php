b5e4c4b070699db7ac91a5c0336af54d5c6c63e8
romanlazko <43849612+romanlazko@users.noreply.github.com>
2019-01-02 22:28:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -24,25 +24,17 @@ function name($token,$user_id,$reply){
     $dbconnect->close();
 }
 function setdisen($user_id){ 
-    $servername="db4free.net: 3306";
-    $username="romanlazko";
+    $servername="78.108.80.117";
+    $username="u178949_vkbot";
     $password="zdraste123";
-    $dbname="promocoder1";
+    $dbname="b178949_vkbot";
     $dbconnect = new mysqli($servername, $username, $password, $dbname);
     $result1 = $dbconnect->query("SELECT disen FROM vkbot WHERE user_id='$user_id'");    
-    while($row = $result1->fetch_assoc()){        
-        if($row['disen']=='1'){
-            $updatename1 = $dbconnect->query("UPDATE `vkbot` SET `disen`='0' WHERE `user_id`='$user_id'");
-            return true;
-            break;
-        }
-        else{
-            return false;
-            break;
-        }
+    while($row = $result1->fetch_assoc()){
+        $updatename1 = $dbconnect->query("UPDATE `vkbot` SET `disen`='0' WHERE `user_id`='$user_id'");        
+        return $row['disen'];
     }   
     $dbconnect->close();
-    
 }
 function update_file($file_url){ 
     $servername="db4free.net: 3306";
@@ -116,35 +108,64 @@ if($type == 'message_new'){
         sendMessage($token,$user_id,select_file());
     }
     else{
-//         if(setdisen($user_id)===true){
-//             $text = str_replace(' ','-',$text);
-//             $url = "https://www.mvcr.cz/clanek/verejna-vyhlaska-oznameni-o-moznosti-prevzit-pisemnost-".$text.".aspx";
+        $setdisen = setdisen($user_id);
+        if($setdisen==='1'){
+            $text = str_replace(' ','-',$text);
+            $url = "https://www.mvcr.cz/clanek/verejna-vyhlaska-oznameni-o-moznosti-prevzit-pisemnost-".$text.".aspx";
             
-//             $url1 ="https://www.mvcr.cz/clanek/verejne-vyhlasky-oamp-verejna-vyhlaska-oznameni-o-moznosti-prevzit-pisemnost-".$text.".aspx";
-//             $urlHeaders = @get_headers($url);
-//             $urlHeaders1 = @get_headers($url1);
-//             $keyboard = [ 
-//                 'one_time' => true, 
-//                 'buttons' => keyboard("1",'Начать','positive')
-//             ];
+            $url1 ="https://www.mvcr.cz/clanek/verejne-vyhlasky-oamp-verejna-vyhlaska-oznameni-o-moznosti-prevzit-pisemnost-".$text.".aspx";
+            $urlHeaders = @get_headers($url);
+            $urlHeaders1 = @get_headers($url1);
+            $keyboard = [ 
+                'one_time' => true, 
+                'buttons' => keyboard("1",iconv( 'cp1251','utf-8' , 'Начать'),'positive')
+            ];
 
-//             if(strpos($urlHeaders[0], '200')) {
-//                 sendKeyboard($token,$user_id,$url,$keyboard);
-//             } elseif(strpos($urlHeaders1[0], '200')) {
-//                  sendKeyboard($token,$user_id,$url1,$keyboard);
-//             } else{
-//                 sendKeyboard($token,$user_id,'Нет ссылки',$keyboard);
-//             }
-//             //sendMessage($token,$user_id,'проверка');
-//         }else{
-//             $reply="Прости, я не понимаю ".$text. ")
-//             \nПопробуй еще раз!";
-//             $keyboard = [ 
-//                 'one_time' => true, 
-//                 'buttons' => keyboard("1",'Начать','positive')
-//             ];
-//             sendKeyboard($token,$user_id,$reply,$keyboard);
-//         }
+            if(strpos($urlHeaders[0], '200')) {
+                $reply = $first_name.iconv( 'cp1251','utf-8' ,', на имя : ').$text.iconv( 'cp1251','utf-8' ,' обнаружено письмо.
+Ознакомиться с ним Вы можете по ссылке: 
+').$url;
+                sendKeyboard($token,$user_id,$reply,$keyboard);
+            } elseif(strpos($urlHeaders1[0], '200')) {
+                 $reply = $first_name.iconv( 'cp1251','utf-8' ,', на имя : ').$text.iconv( 'cp1251','utf-8' ,' обнаружено письмо.
+Ознакомиться с ним Вы можете по ссылке: 
+').$url1;
+                 sendKeyboard($token,$user_id,$reply,$keyboard);
+            } else{
+                $reply = $first_name.iconv( 'cp1251','utf-8' ,', на имя : ').$text.iconv( 'cp1251','utf-8' ,' письма не обнаружено');
+                sendKeyboard($token,$user_id,$reply,$keyboard);
+            }
+        }
+        elseif($setdisen==='2'){
+            $visa_url=select_file();
+            $handle = fopen($visa_url, "r");
+            //sendMessage($token,$user_id,$visa_url);
+            //if (($handle = fopen($visa_url, "r")) !== FALSE) {
+
+                while (($visanum = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                     $cutvisa = substr($visanum[1], 2);
+                     $cutvisa1 = substr($cutvisa, 0, -2);
+                     if($cutvisa1===$text){
+                          sendMessage($token,$user_id,$cutvisa1.' first method');
+                          break;
+                     }
+                     elseif(strripos($visanum[1], $text)){
+                          sendMessage($token,$user_id,iconv( 'ISO-8859-2','utf-8' ,$visanum[1]).' second method');
+                          break;
+                     }
+                     
+                
+                }
+
+                
+                fclose($handle);
+            //} 
+                
+        }
+        else{
+            header("HTTP/1.1 200 OK");
+            echo 'ok';            
+        }
     }
 }
 function sendKeyboard($token,$user_id,$reply,$keyboard){
