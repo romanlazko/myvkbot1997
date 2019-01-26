841955000f98422038af12946e9a78724c5338f1
romanlazko <43849612+romanlazko@users.noreply.github.com>
2019-01-26 15:35:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -109,7 +109,7 @@ $text = $data['object']['text'];
 // //$last_name = iconv( 'utf-8','cp1251' , $userInfo['response'][0]['last_name']);
 // $first_name = $userInfo['response'][0]['first_name'];
 // $last_name = $userInfo['response'][0]['last_name'];
-$setdisen = setdisen($user_id);
+//$setdisen = setdisen($user_id);
 //$setdisen = 1;
 if($type == 'confirmation'){
     echo $confirmationToken;
